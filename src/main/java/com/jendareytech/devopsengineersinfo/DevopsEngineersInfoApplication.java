66deeae5b0fdcacd92186c308b1ad55d4908bd8b
BrianKolawole <dammy.brian@gmail.com>
2023-09-24 01:50:45 +0000

    my first commit

---
+++ src/main/java/com/jendareytech/devopsengineersinfo/DevopsEngineersInfoApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.devopsengineersinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsEngineersInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsEngineersInfoApplication.class, args);
+	}
+
+}
